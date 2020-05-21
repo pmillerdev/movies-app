2d52536f25d7ebc0ef0b5858dc7f601a3d3bfd7e
pmillerdev <pmillerdev@gmail.com>
2020-05-21 14:06:40 +0100

    fix: upgrade to latest dependencies and remove console errors

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -8,63 +8,90 @@ class Movie extends Component {
     super(props);
 
     this.state = {
-      favourited: false
+      favourited: false,
     };
   }
 
   handleAddFavourite = () => {
     this.setState({
-      favourited: !this.state.favourited
+      favourited: !this.state.favourited,
     });
     this.props.addToFavourites(this.props.movie);
   };
 
   handleRemoveFavourite = () => {
     this.setState({
-      favourited: !this.state.favourited
+      favourited: !this.state.favourited,
     });
     this.props.removeFromFavourites(this.props.movie);
   };
 
   displayFavouriteIcon = () => {
-    return !this.state.favourited ? 
-    (<span className="glyphicon glyphicon-heart-empty"
-      onClick={this.handleAddFavourite}
-      style={{ color: "gold", cursor: "pointer"}}
-      title="Add to favourites"
-    ></span>) :
-    (<span className="glyphicon glyphicon-heart"
-      onClick={this.handleRemoveFavourite}
-      style={{ color: "gold", cursor: "pointer"}}
-      title="Remove from favourites"
-    ></span>)
+    return !this.state.favourited ? (
+      <span
+        className="glyphicon glyphicon-heart-empty"
+        onClick={this.handleAddFavourite}
+        style={{ color: "gold", cursor: "pointer" }}
+        title="Add to favourites"
+      ></span>
+    ) : (
+      <span
+        className="glyphicon glyphicon-heart"
+        onClick={this.handleRemoveFavourite}
+        style={{ color: "gold", cursor: "pointer" }}
+        title="Remove from favourites"
+      ></span>
+    );
   };
 
-  formatDate = moviedate => {
-     return moviedate.split("-").reverse().join("/");
+  formatDate = (moviedate) => {
+    return moviedate.split("-").reverse().join("/");
   };
 
   render() {
     const movie = this.props.movie;
-    return(
+    return (
       <div className="col-xs-12 col-sm-6 col-md-4">
         <div className="thumbnail">
-          <a href={`https://www.themoviedb.org/movie/${movie.id}`} target="_blank" rel="noopener norefferer"><img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="None" /></a>
+          <a
+            href={`https://www.themoviedb.org/movie/${movie.id}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+              alt="None"
+            />
+          </a>
           <div className="caption">
-            <h3>{movie.title} {!this.props.showFavButton && this.displayFavouriteIcon()}</h3>
-            <p><strong>{this.formatDate(movie.release_date)}</strong></p>
-            <p id="overview" >{movie.overview}</p>            
-            <p>User Score: <span className="badge badge-default"><span className="glyphicon glyphicon-star" aria-hidden="true"></span>{" "}{movie.vote_average}</span></p>
+            <h3>
+              {movie.title}{" "}
+              {!this.props.showFavButton && this.displayFavouriteIcon()}
+            </h3>
+            <p>
+              <strong>{this.formatDate(movie.release_date)}</strong>
+            </p>
+            <p id="overview">{movie.overview}</p>
+            <p>
+              User Score:{" "}
+              <span className="badge badge-default">
+                <span
+                  className="glyphicon glyphicon-star"
+                  aria-hidden="true"
+                ></span>{" "}
+                {movie.vote_average}
+              </span>
+            </p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 Movie.propTypes = {
   movie: PropTypes.object.isRequired,
-  showFavButton: PropTypes.bool
-}
+  showFavButton: PropTypes.bool,
+};
 
 export default connect(null, { addToFavourites, removeFromFavourites })(Movie);
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,42 +4,47 @@ import { Form, FormControl, FormGroup, Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import { API_KEY } from "../secrets";
 import { movies } from "../actions";
-  
+
 class SearchBox extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      query: ""
+      query: "",
     };
   }
 
   handleSearch = (e) => {
-    e.preventDefault()
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&page=1&query=${this.state.query}`
+    e.preventDefault();
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&page=1&query=${this.state.query}`;
     fetch(url, {
-      method: "GET"
-    }).then(response => response.json()).then(jsonObj => { this.props.movies(jsonObj.results)});
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((jsonObj) => {
+        this.props.movies(jsonObj.results);
+      });
   };
 
   render() {
-    return(
+    return (
       <Form inline>
-        <FormGroup
-          controlId="formBasicText"
-        >
+        <FormGroup controlId="formBasicText">
           <FormControl
             type="text"
             placeholder="Name of movie"
-            onChange={(event) => this.setState({
-              query: event.target.value
-            })}
-          />
-          {" "}
-          <Button bsStyle="primary" type="submit" onClick={this.handleSearch}>Search</Button>
+            onChange={(event) =>
+              this.setState({
+                query: event.target.value,
+              })
+            }
+          />{" "}
+          <Button bsstyle="primary" type="submit" onClick={this.handleSearch}>
+            Search
+          </Button>
         </FormGroup>
       </Form>
-    )
+    );
   }
 }
 
