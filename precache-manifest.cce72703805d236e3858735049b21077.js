78b223d00f23e1bf985df4a1c42268d9637c4689
pmillerdev <pmillerdev@gmail.com>
2020-05-21 14:14:08 +0100

    Updates

---
+++ precache-manifest.cce72703805d236e3858735049b21077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3041ca2bc8b1080ba9e81487da23ea",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "3653f82a275944b0b46b",
+    "url": "/movies-app/static/css/main.0cd89400.chunk.css"
+  },
+  {
+    "revision": "4adb2ec9d8e94ed8d987",
+    "url": "/movies-app/static/js/2.2109a647.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movies-app/static/js/2.2109a647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3653f82a275944b0b46b",
+    "url": "/movies-app/static/js/main.ad514422.chunk.js"
+  },
+  {
+    "revision": "2b76498351ee2df7cef7",
+    "url": "/movies-app/static/js/runtime-main.48b6978c.js"
+  }
+]);
